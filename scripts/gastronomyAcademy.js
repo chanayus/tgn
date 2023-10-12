f4e3cef022a0098a4a657147555fbf97eff6ed92
chanayus <chanayus1526@hotmail.com>
2023-10-12 13:01:19 +0700

    Update gastronomyAcademy.js

--- scripts/gastronomyAcademy.js
+++ scripts/gastronomyAcademy.js
@@ -12,9 +12,8 @@ gsap
 
 const initGallery = () => {
   const gallery = document.querySelector("#gallery-marquee");
-  console.log(gallery);
   const childElement = gallery.firstElementChild;
-  console.log(gallery);
+
   if (childElement) {
     for (let i = 0; i <= 3; i++) {
       const duplicatedElement = childElement.cloneNode(true);
