479dd7db01f1eac60751c151f384117628132c5c
chanayus <chanayus1526@hotmail.com>
2024-01-11 19:03:16 +0700

    add new pages

--- scripts/gastronomyTrailAndVisit.js
+++ scripts/gastronomyTrailAndVisit.js
@@ -1,16 +1,23 @@
+const unloadImgSection = document.querySelectorAll(".img-section");
+const loadMoreButton = document.querySelector("#loadmore-button");
+
+loadMoreButton.addEventListener("click", () => {
+  unloadImgSection.forEach((section) => {
+    section.classList.remove("max-md:hidden");
+  });
+  loadMoreButton.remove();
+});
+
 gsap
   .timeline()
   .fromTo(
     "#hero-image img",
     {
       autoAlpha: 1,
-      yPercent: 150,
-      scale: 1.5,
+      width: "0%",
     },
     {
-      autoAlpha: 1,
-      yPercent: 0,
-      scale: 1,
+      width: "100%",
       ease: "expo",
       duration: 1.35,
     },
