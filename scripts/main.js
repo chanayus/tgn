c3a5af931c26d2204840bf5df5fc36ed829b57fa
chanayus <chanayus1526@hotmail.com>
2024-01-17 16:30:47 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,2 @@
 import "./modules/collapsibleMenu.js";
 import "./modules/mobileNavMenu.js";
-
-const navbar = document.querySelector("#navbar");
-
-const pathsGoldTheme = [
-  "educational-research-initiatives",
-  "gastronomy-academy",
-  "gastronomy-fair-and-forum",
-  "media-library",
-  "storytelling-through-food",
-  "food-learning-and-workshop",
-  "gastronomy-trail-and-visit",
-  "thailand-gastronomy-network"
-];
-
-const pathname = window.location.pathname.substring(1).replace(".html", "");
-
-if (pathsGoldTheme.indexOf(pathname) !== -1) {
-  navbar?.classList.add("gold-theme");
-}
-
-window.addEventListener("beforeunload", () => window.scrollTo(0, 0));
