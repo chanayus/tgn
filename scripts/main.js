b2c7b5f571ca5946fc0ce1d3085e0d861e3b2e79
Xhanayus <chanayus1526@gmail.com>
2025-06-02 11:16:23 +0700

    add all event page

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,31 @@
 import "./modules/collapsibleMenu.js";
 import "./modules/mobileNavMenu.js";
+
+export const initMarquee = () => {
+  const gallery = document.querySelector(".marquee");
+
+  if (gallery === null) return;
+
+  const childElement = gallery.firstElementChild;
+
+  if (childElement) {
+    for (let i = 0; i <= 3; i++) {
+      const duplicatedElement = childElement.cloneNode(true);
+      gallery.appendChild(duplicatedElement);
+    }
+  }
+
+  const num = -100;
+  gsap.fromTo(
+    ".marquee-item",
+    { xPercent: 0 },
+    {
+      xPercent: num,
+      repeat: -1,
+      duration: 25,
+      ease: "linear",
+    }
+  );
+};
+
+initMarquee();
